db07e25a4e03a4b93e1d2f4c622b356e0151992e
nandoxscr <nnwwpp77@gmail.com>
2023-09-05 08:29:25 +0700

    init

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2020 ArrowOS
+# Copyright (C) 2023 Project-Elixir
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_ginkgo.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_ginkgo-user \
+    aosp_ginkgo-userdebug \
+    aosp_ginkgo-eng
